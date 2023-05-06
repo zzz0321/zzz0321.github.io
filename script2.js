f494b63592981b4bafdd08f820adc64a3a5c818e
zzz0321 <126553861+zzz0321@users.noreply.github.com>
2023-05-07 01:29:50 +0800

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,57 @@
+function tips(){
+  alert("答案为美食图片对应的地区，如图片热干面所对应的答案为武汉");
+}
+function answer(){
+  var response=prompt("写下你的答案");
+  if(response=="武汉"){
+    alert("right!");
+  }
+  else {
+    alert("wrong！");
+  }
+}
+function answer1(){
+  var response=prompt("写下你的答案");
+  if(response=="广东"){
+    alert("right!");
+  }
+  else {
+    alert("wrong！");
+  }
+}
+function answer2(){
+  var response=prompt("写下你的答案");
+  if(response=="北京"){
+    alert("right!");
+  }
+  else {
+    alert("wrong！");
+  }
+}
+  function answer3(){
+    var response=prompt("写下你的答案");
+    if(response=="桂林"){
+      alert("right!");
+    }
+    else {
+      alert("wrong！");
+    }
+  }
+    function answer4(){
+      var response=prompt("写下你的答案");
+      if(response=="河南"){
+        alert("right!");
+      }
+      else {
+        alert("wrong！");
+      }
+    }
+      function answer5(){
+        var response=prompt("写下你的答案");
+        if(response=="上海"){
+          alert("right!");
+        }
+        else {
+          alert("wrong！");
+        }
+}
